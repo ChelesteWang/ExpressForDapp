4784759feca467e35c2616c310e23a371b5f714e
yuqing521 <1581446178@qq.com>
2020-11-06 18:53:28 +0800

    web3 interface

--- dappdemo/app/routes/web3interface.js
+++ dappdemo/app/routes/web3interface.js
@@ -2,7 +2,12 @@ var express = require("express");
 const Web3 = require("web3");
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 var router = express.Router();
-
+var Contract = {
+  abi: [],
+  address: "0x598a366C768aAA60697cC0c254aC4F57c29D6c07",
+  endpoint: "http://localhost:7545",
+};
+// 区块链用户
 router.get("/getAccounts", function (req, res, next) {
   web3.eth.getAccounts(function (error, result) {
     if (!error) {
@@ -16,10 +21,7 @@ router.get("/getAccounts", function (req, res, next) {
   //   res.render('index', { title: 'Express' });
 });
 
-router.post("/1234", function (req, res, next) {
-  let { msg } = req.body;
-});
-
+// 区块链节点数
 router.get("/getBlockNumber", function (req, res, next) {
   web3.eth.getBlockNumber(function (error, result) {
     if (!error) res.send(JSON.stringify(result));
@@ -27,4 +29,32 @@ router.get("/getBlockNumber", function (req, res, next) {
   });
 });
 
+// 区块链 gas 价格
+router.get("/getGasPrice", function (req, res, next) {
+  web3.eth.getGasPrice(function (error, result) {
+    if (!error) res.send(JSON.stringify(result));
+    else res.send(JSON.stringify(error));
+  });
+});
+
+router.get("/1234", function (req, res, next) {
+  //   let { msg } = req.body;
+  var msg = "helloworld";
+  let data = web3.utils.toHex(msg);
+  var from = "0xa4D36E545C786f9aFf568B369deb2DA9c08583B8";
+  var to = "0x598a366C768aAA60697cC0c254aC4F57c29D6c07";
+  let transaction = {
+    from,
+    to,
+    value: "0x00",
+    data: data,
+  };
+  console.log(transaction);
+  web3.eth.sendTransaction(transaction, (error, hash) => {
+    console.log(hash);
+    var result = web3.utils.toAscii(hash);
+    console.log(result)
+    res.send(result);
+  });
+});
 module.exports = router;
