f4a47e48483871c250c903be393d28a67cec0cd4
yuqing521 <1581446178@qq.com>
2020-11-06 13:11:53 +0800

    commit

--- dapp/routes/index.js
+++ dapp/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/index',function(req,res,next){
+  web3.eth.
+})
+
 module.exports = router;
