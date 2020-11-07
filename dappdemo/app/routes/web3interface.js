3517a40dc5e845a3ccd14f495842436ad3fef728
yuqing521 <1581446178@qq.com>
2020-11-07 09:46:28 +0800

    update Contract

--- dappdemo/app/routes/web3interface.js
+++ dappdemo/app/routes/web3interface.js
@@ -2,11 +2,56 @@ var express = require("express");
 const Web3 = require("web3");
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 var router = express.Router();
+
 var Contract = {
-  abi: [],
+  abi: [
+    {
+      constant: false,
+      inputs: [
+        {
+          name: "newMessage",
+          type: "string",
+        },
+      ],
+      name: "update",
+      outputs: [],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [],
+      name: "message",
+      outputs: [
+        {
+          name: "",
+          type: "string",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          name: "initMessage",
+          type: "string",
+        },
+      ],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "constructor",
+    },
+  ],
   address: "0x598a366C768aAA60697cC0c254aC4F57c29D6c07",
   endpoint: "http://localhost:7545",
 };
+
+var contract_interface = new web3.eth.Contract(Contract.abi, Contract.address);
+var instance = contract_interface;
+
 // 区块链用户
 router.get("/getAccounts", function (req, res, next) {
   web3.eth.getAccounts(function (error, result) {
@@ -37,12 +82,13 @@ router.get("/getGasPrice", function (req, res, next) {
   });
 });
 
-router.post("/sendMessage", function (req, res, next) {
-  //   let { msg, from, to } = req.body;
-  var msg = "helloworld";
-  let data = web3.utils.toHex(msg);
+// 上传交易信息(发送数据)
+router.get("/sendMessage", function (req, res, next) {
+  // let { msg, from, to } = req.body;
+  var msg = "5258661615151";
   var from = "0xa4D36E545C786f9aFf568B369deb2DA9c08583B8";
   var to = "0x598a366C768aAA60697cC0c254aC4F57c29D6c07";
+  let data = web3.utils.toHex(msg);
   let transaction = {
     from,
     to,
@@ -52,11 +98,59 @@ router.post("/sendMessage", function (req, res, next) {
   console.log(transaction);
   web3.eth.sendTransaction(transaction, (error, hash) => {
     console.log(hash);
-    var result = web3.utils.toAscii(hash);
-    console.log(result);
-    res.send(result);
+    res.send(hash);
+  });
+});
+
+// abi 接口调用 暂不能使用
+router.get("/sendMessage1", function (req, res, next) {
+  // let { msg, from, to } = req.body;
+  var msg = "helloworld";
+  instance.methods.update(msg).call(
+    {
+      from: web3.eth.accounts[0],
+      gas: 100000,
+      gasPrice: 100000,
+      gasLimit: 100000,
+    },
+    // function (error, txHash) {
+    //   if (error) {
+    //     console.log(error);
+    //   } else {
+    //     console.log(txHash)
+    //     getTransactionReceipt(txHash,function(receipt){
+    //       if(receipt.status){
+    //         console.log({ receipt });
+    //         res.send(receipt)
+    //       }
+    //     })
+    //   }
+    // }
+  );
+  res.send("123456")
+});
+
+// var getTransactionReceipt = function(hash,cb) {
+//   web3.eth.getTransactionReceipt(hash, function (err, receipt) {
+//     if (err) {
+//       error(err);
+//     }
+//     if (receipt !== null) {
+//       if (cb) {
+//         cb(receipt);
+//       }
+//     }
+//   });
+// }
+
+// 获取交易信息(获取数据)
+router.get("/getMessage", function (req, res, next) {
+  var hash =
+    "0x0a9dab32007cb7c0a0235ba106858e63e10768358cdaedb06554a13cdbac7787";
+  web3.eth.getTransaction(hash, (error, result) => {
+    var { input } = result;
+    res.send(input);
   });
 });
 
 module.exports = router;
-
